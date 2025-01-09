9d6291e29cec990d0c5e5e62cbb3fff39eca0e8f
rafaelssucupira <rafaelssucupira@gmail.com>
2025-01-09 08:38:14 -0300

    tentando mais uma vez.

--- utils/cron.js
+++ utils/cron.js
@@ -36,50 +36,12 @@ class Cron extends wwebjs
 	async getContacts()
 		{
 
-			// const allContacts 	= await client.getContacts();
-			// let i = 0
-			// const result = []
-			// for(const data of allContacts) {
-			// 	if(data.name && data.name.includes("@paodiario")) {
-			// 		console.log(data.name)
-			// 		result.push({name : data.name, phone : data.);
-			// 	}
-
-			// }
-			// console.log(result.length);
-
-
 			const allContacts       = await client.getContacts();
 			const result 			= 	allContacts
 								.filter(contact => contact.name && contact.name.includes("@paodiario") && contact.id.server === "c.us")
 								.map(contact => ({name : contact.name, number : contact.number}) )
 
 			return result
-			// console.log(allContacts.length);
-			// const filteredContacts = allContacts.filter(contact => contact.id.server === "c.us" );
-			// console.log(filteredContacts.length);
-
-			// const resultFinally = allContacts.reduce( (acc, current) => {
-			// 	if(current.id.server === "c.us" && current.name && current.name.includes("@paodiairo") === true)
-			// 		{
-			// 			if(acc.includes( current.id._serialized ) === false )
-			// 				{
-			// 					acc.push(current.id._serialized);
-			// 				}
-			// 		}
-
-			// 	return acc;
-			// }, [] )
-
-			// console.log(resultFinally.length);
-
-
-		// 	console.log(filteredContacts.length);
-		// 	const ct =  allContacts
-		// 		.filter(contact => contact.name && contact.name.includes("@paodiario"))
-		// 		.map(contact => ({...contact}) );
-
-		// console.log(ct.length);
 
 		}
 
@@ -107,7 +69,6 @@ class Cron extends wwebjs
 			for(const data of contacts)
 				{
 					await setTimeout(1000);
-					console.log(data.name);
 					await this.sendMessage(data.number, this.#message )
 				}
 
