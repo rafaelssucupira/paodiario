681977f1446b8b6cf7350b8edfbc4f48b5a68eaa
rafaelssucupira <rafaelssucupira@gmail.com>
2026-01-07 15:02:37 -0300

    .

--- utils/client.js
+++ utils/client.js
@@ -4,13 +4,12 @@ const { Client, MessageMedia, LocalAuth } = pkg
 // https://github.com/pedroslopez/whatsapp-web.js/issues/2847
 // https://github.com/pedroslopez/whatsapp-web.js/issues/2789
 const client = new Client({
-    authStrategy: new LocalAuth(),
-	webVersionCache: {
-		type: 'remote',
-		remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
-   }
+    authStrategy: new LocalAuth()
 });
-
+// 	webVersionCache: {
+// 		type: 'remote',
+// 		remotePath: 'https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html',
+//    }
 export {
 	client,
 	MessageMedia
--- utils/cron.js
+++ utils/cron.js
@@ -66,10 +66,12 @@ class Cron extends wwebjs
 	async start(contacts)
 		{
 			await this.getMessage();
+			console.log(this.#message);
 			for(const data of contacts)
 				{
 					await setTimeout(1000);
-					await this.sendMessage(data.number, this.#message )
+					//await this.sendMessage(data.number, this.#message )
+					console.log("message sent to : ", data.number);
 				}
 
 
